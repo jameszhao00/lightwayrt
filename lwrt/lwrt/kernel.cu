834086d82dc71a56db01ac56800e27a591f3e4a2
james <jameszhao00@gmail.com>
2012-10-16 20:11:16 -0400

    SOA output buffer.

--- lwrt/lwrt/kernel.cu
+++ lwrt/lwrt/kernel.cu
@@ -38,10 +38,12 @@ struct Pass
 	int num_iterations;
 	int num_bounces;
 };
+
+#ifndef LW_UNIT_TEST
 surface<void, cudaSurfaceType2D> output_surf;
 GPU_ENTRY void gfx_kernel(Vec3Buffer buffer, const Camera* camera, const Scene* scene, const Pass* pass, int width, int height
 #ifdef LW_CPU
-	, ref::glm::uvec2 xy
+//	, ref::glm::uvec2 xy
 #endif
 	) {
 	ref::glm::uvec2 screen_size(width, height);
@@ -123,8 +125,11 @@ GPU_ENTRY void gfx_kernel(Vec3Buffer buffer, const Camera* camera, const Scene* 
 	}
 	color existing = buffer.get(linid);
 	float existing_weight = (float)pass->iteration_idx / (pass->iteration_idx + pass->num_iterations);
-	color combined = (value / (float)pass->num_iterations) 
-		* (1 - existing_weight) + existing * (existing_weight);
+	color combined = (value / (float)pass->num_iterations) * (1 - existing_weight);
+	if(pass->iteration_idx > 0)
+	{
+		combined = combined + existing * (existing_weight);
+	}
 	buffer.set(linid, combined);
 	color combined_tonemapped = combined / (combined + color(1,1,1));
 	surf2Dwrite(make_float4(combined_tonemapped.x, combined_tonemapped.y, combined_tonemapped.z, 1), output_surf, xy.x*sizeof(float4), xy.y);
@@ -161,58 +166,7 @@ void Kernel::execute(int iteration_idx, int iterations, int bounces, int width, 
 	gfx_kernel<<<blocks, threadPerBlock>>>(*buffer, (Camera*)camera_ptr, (Scene*)scene_ptr, (Pass*)pass_ptr, width, height);
 	CUDA_CHECK_RETURN(cudaGraphicsUnmapResources(1, &framebuffer_resource));
 }
-/*
-const int WIDTH = 600;
-const int HEIGHT = 600;
-
-int main(int argc, char* const argv[]) {
-#ifdef LW_UNIT_TEST
-	Catch::Main( argc, argv );
-#else
-	ref::glm::vec4 *d = NULL;
-	Camera *camera_ptr = NULL;
-	Scene *scene_ptr= NULL;
-	ref::glm::vec4* odata = new ref::glm::vec4[WIDTH * HEIGHT];
-	Pass* pass_ptr = nullptr;
-	Pass pass(0, 600, 4);
-	Camera camera(position<World>(0,7,-5), position<World>(0,0,1));
-	Scene scene;
-	CUDA_CHECK_RETURN(cudaMalloc((void**) &d, sizeof(ref::glm::vec4) * WIDTH * HEIGHT));
-	CUDA_CHECK_RETURN(cudaMalloc((void**) &camera_ptr, sizeof(Camera)));
-	CUDA_CHECK_RETURN(cudaMalloc((void**) &scene_ptr, sizeof(Scene)));
-	CUDA_CHECK_RETURN(cudaMalloc((void**) &pass_ptr, sizeof(Pass)));
-	CUDA_CHECK_RETURN(cudaMemcpy(camera_ptr, &camera, sizeof(Camera), cudaMemcpyHostToDevice));
-	CUDA_CHECK_RETURN(cudaMemcpy(scene_ptr, &scene, sizeof(Scene), cudaMemcpyHostToDevice));
-	CUDA_CHECK_RETURN(cudaMemcpy(pass_ptr, &pass, sizeof(Pass), cudaMemcpyHostToDevice));
-	dim3 threadPerBlock(8, 8, 1);
-	dim3 blocks((unsigned int)ceil(WIDTH / (float)threadPerBlock.x), (unsigned int)ceil(HEIGHT / (float)threadPerBlock.y), 1);
-	gfx_kernel<<<blocks, threadPerBlock>>>(d, camera_ptr, scene_ptr, pass_ptr, WIDTH, HEIGHT);
-
-	CUDA_CHECK_RETURN(cudaThreadSynchronize());	// Wait for the GPU launched work to complete
-	CUDA_CHECK_RETURN(cudaGetLastError());
-	CUDA_CHECK_RETURN(cudaMemcpy((void*)odata, (void*)d, sizeof(float4) * WIDTH * HEIGHT, cudaMemcpyDeviceToHost));
-
-	CUDA_CHECK_RETURN(cudaFree((void*) d));
-	CUDA_CHECK_RETURN(cudaDeviceReset());
-
-	bitmap_image image(WIDTH, HEIGHT);
-	for(int j = 0; j < HEIGHT; j++)
-	{
-		for(int i = 0; i < WIDTH; i++)
-		{
-			ref::glm::vec4 col = odata[j * WIDTH + i];
-			col /= 1.f + col;
-			col *= 255;
-			ref::glm::uvec4 ucolor(col);
-			image.set_pixel(i, j, ucolor.x, ucolor.y, ucolor.z);
-		}
-	}
-	image.save_image("out.bmp");
-	
 #endif
-	return 0;
-}
-*/
 #ifdef LW_UNIT_TEST
 //tests
 TEST_CASE("camera/camera_ray", "standard camera_ray") 
@@ -280,5 +234,10 @@ TEST_CASE("intersect/intersect_plane", "hits plane everywhere")
 		REQUIRE(hit.normal.y == 1.f);
 	}
 }
-
+TEST_CASE("math/color", "color math works") 
+{
+	REQUIRE(all_equal(color(1,1,1) / 2, color(0.5f, 0.5f, 0.5f)));
+	//REQUIRE(all_equal(color(1,1,1) + 2, color(3,3,3)));
+	REQUIRE(all_equal(color(1,1,1) / color(2,2,2), color(0.5f, 0.5f, 0.5f)));
+}
 #endif
--- lwrt/lwrt/util.h
+++ lwrt/lwrt/util.h
@@ -57,6 +57,10 @@ struct v3
 	GPU_CPU v3(float x, float y, float z) : x(x), y(y), z(z) { }
 	GPU_CPU void set(float v) { x=v;y=v;z=v; }
 };
+GPU_CPU bool all_equal(const v3& a, const v3& b, float epsilon = 0.00000001f)
+{
+	return close_to(a.x, b.x, epsilon) && close_to(a.y, b.y, epsilon) && close_to(a.z, b.z, epsilon);
+}
 GPU_CPU v3 clamp(const v3& v, float min_val, float max_val)
 {
 	return v3(
@@ -87,6 +91,9 @@ GPU_CPU v3 v3_sub(const v3& lhs, const v3& rhs) { return v3_add(lhs, v3_neg(rhs)
 GPU_CPU v3 v3_mul(const v3& v, float m) { return v3(v.x*m,v.y*m,v.z*m); }
 GPU_CPU v3 v3_mul(const v3& a, const v3& b) { return v3(a.x*b.x,a.y*b.y,a.z*b.z); }
 
+GPU_CPU v3 v3_div(const v3& v, float m) { float inv = 1/m; return v3_mul(v, inv); }
+GPU_CPU v3 v3_div(const v3& a, const v3& b) { v3 inv(1/b.x, 1/b.y, 1/b.z); return v3_mul(a, inv); }
+
 GPU_CPU float v3_len(const v3& v) { return sqrt(dot(v, v)); }
 GPU_CPU v3 v3_normalized(const v3& v) { return v3_mul(v, (1.f / v3_len(v))); }
 
@@ -104,7 +111,7 @@ struct color : v3
 	GPU_CPU color operator-(const color& rhs) const { return v3_sub(*this, rhs); }
 	GPU_CPU color operator*(float rhs) const { return v3_mul(*this, rhs); }
 	GPU_CPU color operator/(float rhs) const { return *this * (1.f/rhs); }
-	GPU_CPU color operator/(const color& rhs) const { return v3_mul(*this, rhs); }
+	GPU_CPU color operator/(const color& rhs) const { return v3_div(*this, rhs); }
 	GPU_CPU bool is_black() const { return x == 0 && y == 0 && z == 0; }
 };
 template<CoordinateSystem CS>
