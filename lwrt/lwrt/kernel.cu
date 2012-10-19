b0e0a89d4629a30c6e5a560e37018170b4e92987
james <jameszhao00@gmail.com>
2012-10-19 19:25:31 -0400

    removed We

--- lwrt/lwrt/kernel.cu
+++ lwrt/lwrt/kernel.cu
@@ -148,9 +148,7 @@ GPU_ENTRY void gfx_kernel(Vec3Buffer buffer, const Camera* camera, const Scene* 
 							float costheta_shadow_lv = dot(light_to_eye_shadow_ray.dir, light_vn.normal);
 							float d = (light_vn.position - camera->eye).length();
 							float g = costheta_shadow_ev * costheta_shadow_lv / (d * d);
-							float a = 2 * powf(tan(0.5 * (camera->fovy / 180) * PI), 2);
-							float we = 1 / (a * costheta_shadow_ev * costheta_shadow_ev * costheta_shadow_ev);
-							color value = light_throughput * light_vn.material.brdf() * g * we;	
+							color value = light_throughput * light_vn.material.brdf() * g;	
 							buffer.elementwise_atomic_add(uv.y * width + uv.x, value);
 							
 						}
